7f215ca8000ebeb06af31bd6a512c6f81d45b396
thomaszhwang <thomaswang@thomaswang-mbp.local>
2015-01-08 23:47:22 -0800

    update task display

--- js/main.js
+++ js/main.js
@@ -19,13 +19,31 @@ function load_tasks() {
 
                 var new_task = $(sprintf('\
                     <div class="task">\
+                        <div></div>\
                         <p class="title">%(task_title)s</p>\
-                        <p class="next_step">NEXT STEP: %(task_next_step)s</p>\
-                        <p class="impact">IMPACT: %(task_impact)s</p>\
+                        <p class="next_step">NEXT STEP: <span>%(task_next_step)s</span></p>\
+                        <p class="impact">IMPACT: <span>%(task_impact)s</span></p>\
                     </div>\
                 ', the_task));
 
-                new_task.css('border-bottom', '1px solid gray');
+                new_task.css({
+                    'border-bottom': '1px solid gray',
+                    'cursor': 'pointer'
+                });
+                new_task.children("div").css({
+                    'position': 'absolute',
+                    'left': '0px',
+                    'height': '100%',
+                    'width': '5px',
+                    'background-color': 'white'
+                })
+
+                new_task.mouseenter(function() {
+                    $(this).children('div').css("background-color", "#ea755d");
+                });
+                new_task.mouseleave(function() {
+                   $(this).children('div').css("background-color", "white");
+                })
 
                 $('#tasks > div:first-child').append(new_task);
             }
